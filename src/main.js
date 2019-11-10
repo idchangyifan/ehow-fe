53aaf3214de831e5fc0e2537b23fcee495c97e9b
ChangYifan <changyifan123@qq.com>
2019-11-11 01:04:41 +0800

    增加路由在main.js中 拆分app.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,28 @@ import App from './App.vue'
 import VueResource from "vue-resource"
 import $ from 'jquery';
 Vue.config.productionTip = false;
-
-
+import VueRouter from 'vue-router'
+import Project from './components/Project'
 Vue.use(VueResource);
+Vue.use(VueRouter)
+
+
+
+const Foo = { template: '<div>foo</div>' }
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/project', component: Project }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app');
 
 Vue.use(VueResource);
-
+Vue.use(VueRouter)
